e904014a55117c2f61a18214fcce03636f3a3b95
Brennen Davis <brennendavis@gmail.com>
2017-10-29 02:04:51 -0600

    Fixing Repo to not include some files

--- config/database.js
+++
@@ -1,9 +0,0 @@
-/*jshint esversion: 6 */
-
-// jscs:disable requireTrailingComma
-const crypto = require('crypto').randomBytes(256).toString('hex');
-
-module.exports = {
-  database: process.env.DATABASE || 'mongodb://localhost:27017/makeupapp',
-  secret: crypto
-};
